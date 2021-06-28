bf65c9024a6c77949ee91019b08a77acecacf118
Yinnan Wu <Yinnan.Wu@tum.de>
2021-06-28 11:27:26 +0200

    improve runtime performs

---
+++ src/dictionary_strategy.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy import special
+
+
+class Strategy(object):
+    def __init__(self):
+        pass
+
+
+class Herimite_strategy(Strategy):
+
+    def __init__(self):
+        H0 = special.hermite(0, monic=True)
+        H1 = special.hermite(1, monic=True)
+        H2 = special.hermite(2, monic=True)
+        H3 = special.hermite(3, monic=True)
+        H4 = special.hermite(4, monic=True)
+        self.D = lambda x, y: [H0(x) * H0(y), H1(x) * H0(y), H2(x) * H0(y), H3(x) * H0(y), H4(x) * H0(y),
+                          H0(x) * H1(y), H1(x) * H1(y), H2(x) * H1(y), H3(x) * H1(y), H4(x) * H1(y),
+                          H0(x) * H2(y), H1(x) * H2(y), H2(x) * H2(y), H3(x) * H2(y), H4(x) * H2(y),
+                          H0(x) * H3(y), H1(x) * H3(y), H2(x) * H3(y), H3(x) * H3(y), H4(x) * H3(y),
+                          H0(x) * H4(y), H1(x) * H4(y), H2(x) * H4(y), H3(x) * H4(y), H4(x) * H4(y)]
+
+    def dictionary(self, xm):
+        dictionary = self.D(xm[0], xm[1])
+
+        return np.array([dictionary])
--- src/simpleEDMD.py
+++ src/simpleEDMD.py
@@ -2,68 +2,60 @@ import numpy as np
 import pandas as pd
 import scipy.linalg
 from scipy import special
+import src.dictionary_strategy as d_strategy
 
 class simpleEDMD:
-    
+
     def __init__(self, X, Y):
-        
+
         self.X = X
         self.Y = Y
-        
-        #self.dictionary 
-   
+
+        # self.dictionary
 
     def compute_koopman_operator(self):
-         #dimension and data format specificed to 4.1.1
-         #needs GENERALIZATION
-        
-  
-        G = np.zeros((25,25)) 
-        A = np.zeros((25,25))
+        # dimension and data format specificed to 4.1.1
+        # needs GENERALIZATION
+
+        G = np.zeros((25, 25))
+        A = np.zeros((25, 25))
 
         for m in range(len(self.X)):
             psi_xm = self.dictionary_Hermite_poly(self.X[m])
             psi_ym = self.dictionary_Hermite_poly(self.Y[m])
-            
-            G += psi_xm.T@psi_xm
-            A += psi_ym.T@psi_xm
+
+            G += psi_xm.T @ psi_xm
+            A += psi_xm.T @ psi_ym
         G /= len(self.X)
         A /= len(self.X)
 
         # compute koopman matrix
-        self.koopman_matrix = np.linalg.pinv(G)@A
-        
+        self.koopman_matrix = np.linalg.pinv(G) @ A
+
         # compute koopman eigenvalues
         self.koopman_eigenvalues, self.left_eigenvectors, self.right_eigenvectors = self.sort_eig(self.koopman_matrix)
-        #self.left_eigenvectors = np.linalg.pinv(self.right_eigenvectors)  # w_star.T
-        
+        # self.left_eigenvectors = np.linalg.pinv(self.right_eigenvectors)  # w_star.T
+
         # compute koopman eigenfunctions
-        
+
         # compute koopman modes
-        
-        
+
     def dictionary_Hermite_poly(self, xm):
-        
-        dictionary = []
-        
-        for j in range(0,5):
-            Hx2 = special.hermite(j, monic=True)
-            for i in range(0,5):
-                Hx1 = special.hermite(i, monic=True)
-                dictionary.append(Hx1(xm[0])*Hx2(xm[1]))
-        return np.array([dictionary])
-    
+
+        # dictionary = []
+        #
+        # for j in range(0, 5):
+        #     Hx2 = special.hermite(j, monic=True)
+        #     for i in range(0, 5):
+        #         Hx1 = special.hermite(i, monic=True)
+        #         dictionary.append(Hx1(xm[0]) * Hx2(xm[1]))
+        # return np.array([dictionary])
+
+        h_strategy = d_strategy.Herimite_strategy()
+        return h_strategy.dictionary(xm)
+
     def sort_eig(self, matrix):
 
-        eig_vals, eig_lvecs, eig_rvecs = scipy.linalg.eig(matrix, left= True)
+        eig_vals, eig_lvecs, eig_rvecs = scipy.linalg.eig(matrix, left=True, right=True)
         ind = eig_vals.argsort()[::-1]
         return (eig_vals[ind], eig_lvecs[:, ind].T, eig_rvecs[:, ind])
-                
-                
-            
-        
-        
-
-
-    
-    
